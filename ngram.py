c1abcf3a93d444f2a8ba9f6d9b8d31e838299c0e
Michelle <michmac202@gmail.com>
2016-11-16 20:20:24 -0800

    Implemented basic n-gram generation

--- ngram.py
+++ ngram.py
@@ -1,4 +1,6 @@
 import nltk
+import random
+import collections
 from nltk.util import ngrams
 from collections import Counter
 
@@ -9,14 +11,38 @@ def generate_grams(words, n):
         gramList.append(' '.join(str(i) for i in ngram))
     return gramList
 
-def get_gram(prevWord, freqTable):
+def create_dict(grams, n):
+	gramDict = collections.defaultdict(list)
+	for gram in grams:
+		words = gram.split()
 
+		key = ()
+		for i in range(n-1):
+			key = key + (words[i],)
 
-bigrams = generate_grams('one two three four water bottle give me the water bottle'.split(' '), 2)
-freqTable = Counter(bigrams)
+		print(key)
+		gramDict[key].append(words[n-1])
+		print(words[n-1])
+	return gramDict
+
+def generate_word(key, gramDict):
+	return random.choice(gramDict[key])
+
+n = 3
+grams = generate_grams('one two three four the water bottle give me the water bottle'.split(' '), n)
+freqTable = Counter(grams)
 print(freqTable)
 
-# Generate text
-startWord = random.choice(bigrams)
-for i in range(25):
-	# Do something
+gramDict = create_dict(grams, n)
+print(gramDict)
+
+prevChunk = random.choice(gramDict.keys())
+sentence = [prevChunk[i] for i in range(n-1)]
+
+for i in range(5):
+	nextWord = generate_word(prevChunk, gramDict)
+	sentence.append(nextWord)
+	prevChunk = prevChunk[1:] + (nextWord,)
+print(' '.join(sentence))
+	
+	
