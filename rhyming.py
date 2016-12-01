5ed90b560004cfe5077a9a4ae10ef3a4f1075d13
Michelle <michmac202@gmail.com>
2016-11-30 22:25:29 -0800

    Get syllable

--- rhyming.py
+++ rhyming.py
@@ -9,56 +9,77 @@ extraDict = fmdict.extraDict
 
 phrase_len = 2
 
-# Given 2 pronunciations, do these words rhyme?
-
-def getRhymeScore(pron1, pron2):
-	return 1
-
-def getPron(word):
-	# Check fm dict
-	if word.lower() in extraDict.keys():
-		return [extraDict[word.lower()]]
-	# Look in cmudict
-	if word.lower() in mainDict.keys():
-		return mainDict[word.lower()]
-	return None
-
-def processPhrase(phrase):
-	tokens = wordpunct_tokenize(sentence)
-	punct = ['.', ',', '!', ':', ';']
-	words = [word for word in tokens if word not in punct]
-
-	# List of lists of possible pronunciations for each sound in phrase
-	phraseSounds = []
-
-	# For each word in phrase
-	for word in words:
-		pron = getPron(word)
-		if pron is None: continue
-		for soundIndex in range(len(pron[0])):
-			soundList = []
-			for p in pron:
-				sound = p[soundIndex]
-				if sound not in soundList:
-					soundList.append(sound)
-			phraseSounds.append(soundList)
+# Looks for stressed syllables
+def getSyllables(word):
+	syllables = []
+	currentSyl = []
+	for sounds in mainDict[word.lower()]:
+		print(sounds)
+		for sound in sounds:
+			print(sound)
+			currentSyl.append(sound)
+			if (sound[-1]).isdigit():
+				syllables.append(currentSyl)
+				currentSyl = []
 
 
-	for s in phraseSounds:
-		print s
+	if currentSyl is not []:
+		lastSyl = syllables[-1]
+		for sound in currentSyl:
+			lastSyl.append(sound)
+		syllables[-1] = lastSyl
+	return syllables
+
+print(getSyllables("table"))
+
+# def getRhymeScore(pron1, pron2):
+# 	return 1
+
+# def getPron(word):
+# 	# Check fm dict
+# 	if word.lower() in extraDict.keys():
+# 		return [extraDict[word.lower()]]
+# 	# Look in cmudict
+# 	if word.lower() in mainDict.keys():
+# 		return mainDict[word.lower()]
+# 	return None
+
+# def processPhrase(phrase):
+# 	tokens = wordpunct_tokenize(sentence)
+# 	punct = ['.', ',', '!', ':', ';']
+# 	words = [word for word in tokens if word not in punct]
+
+# 	# List of lists of possible pronunciations for each sound in phrase
+# 	phraseSounds = []
+
+# 	# For each word in phrase
+# 	for word in words:
+# 		pron = getPron(word)
+# 		if pron is None: continue
+# 		for soundIndex in range(len(pron[0])):
+# 			soundList = []
+# 			for p in pron:
+# 				sound = p[soundIndex]
+# 				if sound not in soundList:
+# 					soundList.append(sound)
+# 			phraseSounds.append(soundList)
+
+
+# 	for s in phraseSounds:
+# 		print s
 
 
 
-def processCorpus():
-	lines = sentence.splitlines()
-	phrase = ''
-	for i in range(len(lines)):
-		phrase = phrase + lines[i]
-		if ((i + 1) % phrase_len is 0):
-			processPhrase(phrase)
-			phrase = ''
+# def processCorpus():
+# 	lines = sentence.splitlines()
+# 	phrase = ''
+# 	for i in range(len(lines)):
+# 		phrase = phrase + lines[i]
+# 		if ((i + 1) % phrase_len is 0):
+# 			processPhrase(phrase)
+# 			phrase = ''
 
-processCorpus()
+# processCorpus()
 
 # # Extract list of words
 
