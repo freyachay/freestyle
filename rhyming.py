4a40763b8c0ca6cc9c0ac24ad697dab2caef7fdf
Michelle <michmac202@gmail.com>
2016-11-30 17:44:54 -0800

    Extracts pairs of word, prounuciation

--- rhyming.py
+++ rhyming.py
@@ -1,13 +1,36 @@
 import nltk
+import fmdict
 from nltk.corpus import cmudict
 from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize
 
-sentence = "Happily I am sitting, at a table with! a water bottle \n Merrily we go"
+sentence = "Unawareness I am sitting, at a table with! a water bottle \n Merrily we go"
+mainDict = cmudict.dict()
+extraDict = fmdict.extraDict
 
+# Given 2 pronunciations, do these words rhyme?
+def rhymeScore(pron1, pron2):
+
+
+
+# Extract list of words
 tokens = wordpunct_tokenize(sentence)
-print(tokens)
 punct = ['.', ',', '!', ':', ';']
+words = [word for word in tokens if word not in punct]
 
-filtered = [word for word in tokens if word not in punct]
+pairs = []
+for w in words:
+	# Check fm dict
+	if w.lower() in extraDict.keys():
+		pron = extraDict[w.lower()]
+		pairs.append((w, pron))
+		continue
 
-print(filtered)
+	# Look in cmudict
+	if w.lower() in mainDict.keys():
+		pron = mainDict[w.lower()]
+		pairs.append((w, pron))
+
+
+
+
+for pair in pairs: print(pair)
