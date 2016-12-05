6b0166c666bc0b807427a4561823d8d3ad7ae5d0
Michelle <michmac202@gmail.com>
2016-12-04 22:57:12 -0800

    About to run greedy, commented out constants in constants.py for search

--- constants.py
+++ constants.py
@@ -1,18 +1,39 @@
 
 styleNames = ['Chance']
 
+
+# *********** Greedy
+
 # For gram model (used in model and generation)
 n = 2
 
 # Number of lines per phrase (used in model and generation)
-phraseLen = 1
+phraseLen = 2
 
 # Number of phrases to generate (used in generation)
-numPhrases = 2
+numPhrases = 40
 
 # How probable must a rhyme be to count as needing to rhyme? (used in generation)
-rhymeThresh = 0.05 
+rhymeThresh = 0.05
 
-# Constants for generation by search
-rhymeWeight = 10
-pruningConstant = 10
+
+
+
+
+# ********** Search
+
+# # For gram model (used in model and generation)
+# n = 2
+
+# # Number of lines per phrase (used in model and generation)
+# phraseLen = 1
+
+# # Number of phrases to generate (used in generation)
+# numPhrases = 2
+
+# # How probable must a rhyme be to count as needing to rhyme? (used in generation)
+# rhymeThresh = 0.05 
+
+# # Constants for generation by search
+# rhymeWeight = 10
+# pruningConstant = 10
