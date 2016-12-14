4376a3ae11ec06ac5dee177c1eecca0d6d767612
Freya Chay <freyac@stanford.edu>
2016-12-14 13:47:49 -0800

    trying to pull from master

--- constants.py
+++ constants.py
@@ -4,20 +4,6 @@ styleNames = ['Hamilton']
 
 # *********** Greedy
 
-# # For gram model (used in model and generation)
-
-# n = 2
-
-# # Number of lines per phrase (used in model and generation)
-# phraseLen = 2
-
-# # Number of phrases to generate (used in generation)
-# numPhrases = 3
-
-
-
-# ********** Search
-
 # For gram model (used in model and generation)
 n = 2
 
@@ -25,12 +11,25 @@ n = 2
 phraseLen = 2
 
 # Number of phrases to generate (used in generation)
-numPhrases = 1
+numPhrases = 2
 
-maxLineLen = 8
 
-# Pruning -- how many of each do we branch?
-pruneFluency = 5
-pruneRhyming = 5
 
-lastSylWeight = 3
+# ********** Search
+
+# # For gram model (used in model and generation)
+# n = 2
+
+# # Number of lines per phrase (used in model and generation)
+# phraseLen = 2
+
+# # Number of phrases to generate (used in generation)
+# numPhrases = 1
+
+# maxLineLen = 8
+
+# # Pruning -- how many of each do we branch?
+# pruneFluency = 5
+# pruneRhyming = 5
+
+# lastSylWeight = 3
