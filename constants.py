80205343979eab0f3588eec6cff5470af9706f06
Michelle <michmac202@gmail.com>
2016-12-14 11:37:20 -0800

    committing before pulling

--- constants.py
+++ constants.py
@@ -5,32 +5,31 @@ styleNames = ['Hamilton']
 # *********** Greedy
 
 # For gram model (used in model and generation)
+n = 2
 
+# Number of lines per phrase (used in model and generation)
+phraseLen = 2
+
+# Number of phrases to generate (used in generation)
+numPhrases = 2
+
+
+
+# ********** Search
+
+# # For gram model (used in model and generation)
 # n = 2
 
 # # Number of lines per phrase (used in model and generation)
 # phraseLen = 2
 
 # # Number of phrases to generate (used in generation)
-# numPhrases = 3
+# numPhrases = 1
 
+# maxLineLen = 8
 
+# # Pruning -- how many of each do we branch?
+# pruneFluency = 5
+# pruneRhyming = 5
 
-# ********** Search
-
-# For gram model (used in model and generation)
-n = 2
-
-# Number of lines per phrase (used in model and generation)
-phraseLen = 1
-
-# Number of phrases to generate (used in generation)
-numPhrases = 1
-
-maxLineLen = 8
-
-# Pruning -- how many of each do we branch?
-pruneFluency = 5
-pruneRhyming = 5
-
-lastSylWeight = 3
+# lastSylWeight = 3
